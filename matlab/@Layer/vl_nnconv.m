b4a8c874e5e2ef318aefa8c2eee34b8971a62ae5
Joao Henriques <jotaf98@hotmail.com>
2017-03-30 19:53:17 +0100

    even more documentation

--- matlab/@Layer/vl_nnconv.m
+++ matlab/@Layer/vl_nnconv.m
@@ -1,15 +1,39 @@
 function layer = vl_nnconv(varargin)
 %VL_NNCONV
-%   Setup a conv layer: if there is a 'size' argument,
-%   automatically initializes randomized Params for the filters.
+%   Y = Layer.vl_nnconv(X, F, B) computes the convolution of the image X
+%   with the filter bank F and biases B. See help vl_nnconv for more
+%   details.
 %
-%   The 'weightScale' argument specifies the initialization scale (or
-%   'xavier' for Xavier initialization, which is the default).
+%   This method overloads MatConvNet's vl_nnconv function for Layer
+%   objects, so that instead of executing vl_nnconv, a new Layer object is
+%   returned. X, F and B can be other Layers, including Params, or
+%   constants.
 %
-%   Also handles 'hasBias' (initialize biases), 'learningRate' and
-%   'weightDecay' arguments for the Params.
+%   In addition to those defined by MatConvNet's vl_nnconv, the overloaded
+%   VL_NNCONV(..., 'option', value, ...) accepts the following options:
+%
+%   `size`:: []
+%     If non-empty, automatically sets F and B to new Param objects,
+%     initialized with random filters and zero biases of the given size.
+%     The filters are normally-distributed, with Xavier scaling by default.
+%     When using this syntax, F and B must be omitted.
+%
+%   `hasBias`:: true
+%     Allows disabling the creation of the bias Param in the syntax above.
+%
+%   `weightScale`:: 'xavier'
+%     If set to a real number, scales the filters by that number instead of
+%     using Xavier initialization.
+%
+%   `learningRate`:: 1
+%     Factor used to adjust the created Params' learning rate. Can specify
+%     separate learning rates for F and B with a 2-elements vector.
+%
+%   `weightDecay`:: 1
+%     Factor used to adjust the created Params' weight decay. Can specify
+%     separate weight decays for F and B with a 2-elements vector.
 
-% Copyright (C) 2016 Joao F. Henriques.
+% Copyright (C) 2016-2017 Joao F. Henriques.
 % All rights reserved.
 %
 % This file is part of the VLFeat library and is made available under
--- matlab/@Layer/vl_nnconvt.m
+++ matlab/@Layer/vl_nnconvt.m
@@ -1,7 +1,19 @@
 function layer = vl_nnconvt(varargin)
 %VL_NNCONVT
-%   Setup a conv-transpose layer. Simply create a conv layer first, then
-%   switch the function handle.
+%   Y = Layer.vl_nnconvt(X, F, B) computes the convolution-transpose of the
+%   image X with the filter bank F and biases B. See help vl_nnconvt for
+%   more details.
+%
+%   This method overloads MatConvNet's vl_nnconvt function for Layer
+%   objects, so that instead of executing vl_nnconvt, a new Layer object is
+%   returned. X, F and B can be other Layers, including Params, or
+%   constants.
+%
+%   In addition to those defined by MatConvNet's vl_nnconvt, the overloaded
+%   VL_NNCONVT(..., 'option', value, ...) accepts the same options as
+%   Layer.vl_nnconv.
+
+  % simply create a conv layer first, then switch the function handle
 
   layer = vl_nnconv(varargin) ;
   layer.func = @vl_nnconvt ;
