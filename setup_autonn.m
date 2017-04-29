426668fc1902951c0cefbabc0d0ee89752062bd5
Joao Henriques <jotaf98@hotmail.com>
2017-04-29 17:51:24 +0100

    fix path so that "help autonn" is always available

--- setup_autonn.m
+++ setup_autonn.m
@@ -2,7 +2,7 @@ function setup_autonn()
 %SETUP_AUTONN Sets up AutoNN, by adding its folders to the Matlab path
 
   root = fileparts(mfilename('fullpath')) ;
-  addpath([root '/matlab'], [root '/matlab/wrappers'], [root '/matlab/derivatives']) ;
+  addpath(root, [root '/matlab'], [root '/matlab/wrappers'], [root '/matlab/derivatives']) ;
 
 end
 
