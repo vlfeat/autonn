cff3b078d52ea099f4dc9895ba749a8cd637e591
Joao Henriques <jotaf98@hotmail.com>
2017-07-05 19:15:24 +0100

    add prefetching helper to Dataset

--- matlab/+datasets/Dataset.m
+++ matlab/+datasets/Dataset.m
@@ -12,13 +12,24 @@ classdef Dataset < handle
       args = vl_parseprop(o, args, {'batchSize'}) ;
     end
     
-    function batches = partition(o, idx)
-      % partition indexes into batches (stored in a cell array)
-      batches = cell(1, ceil(numel(idx) / o.batchSize)) ;
+    function batches = partition(o, idx, varargin)
+      % partition indexes into batches (stored in a cell array).
+      % if IDX is a matrix, each column is a distinct sample.
+      opts.prefetch = false ;
+      opts = vl_argparse(opts, varargin) ;
+      
+      if isvector(idx)
+        idx = idx(:)' ;  % ensure row-vector
+      end
+      batches = cell(1, ceil(size(idx,2) / o.batchSize)) ;
       b = 1 ;
-      for start = 1 : o.batchSize : numel(idx)
-        batches{b} = idx(start : min(start + o.batchSize - 1, end)) ;
+      for start = 1 : o.batchSize : size(idx,2)
+        batches{b} = idx(:, start : min(start + o.batchSize - 1, end)) ;
         b = b + 1 ;
+      end
+      
+      if opts.prefetch
+        batches = [batches; batches(1,2:end), {[]}] ;
       end
     end
   end
