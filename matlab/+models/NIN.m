72a40ac15f8c3db1b1e637a912cecdbf38cd91d9
Joao Henriques <jotaf98@hotmail.com>
2018-02-01 17:38:10 +0000

    tweak NIN model

--- matlab/+models/NIN.m
+++ matlab/+models/NIN.m
@@ -12,6 +12,7 @@ function output = NIN(varargin)
   % batchNorm).
   opts.input = Input('name', 'images', 'gpu', true) ;  % default input layer
   opts.numClasses = 10 ;  % number of predicted classes
+  opts.batchNorm = true ;  % whether to use batch normalization
   [opts, convBlockArgs] = vl_argparse(opts, varargin, 'nonrecursive') ;
   
   % build network
@@ -24,7 +25,7 @@ function output = NIN(varargin)
   poolMethod = 'max' ;  % pooling method
   pad = 2 ;  % input padding
   m1 = ninBlock(images, 3, channels, ker, pad, ...
-    poolKer, poolMethod, convBlockArgs, false) ;
+    poolKer, poolMethod, convBlockArgs, opts.batchNorm, false) ;
   outChannels = channels(3) ;  % output channels of the NIN block
   
   % second NIN block
@@ -34,7 +35,7 @@ function output = NIN(varargin)
   poolMethod = 'avg' ;
   pad = 2 ;
   m2 = ninBlock(m1, outChannels, channels, ker, pad, ...
-    poolKer, poolMethod, convBlockArgs, false) ;
+    poolKer, poolMethod, convBlockArgs, opts.batchNorm, false) ;
   outChannels = channels(3) ;
   
   % third NIN block
@@ -44,40 +45,50 @@ function output = NIN(varargin)
   poolMethod = 'avg' ;
   pad = 1 ;
   output = ninBlock(m2, outChannels, channels, ker, pad, ...
-    poolKer, poolMethod, convBlockArgs, true) ;
+    poolKer, poolMethod, convBlockArgs, opts.batchNorm, true) ;
   
   
   % default training options for this network
   defaults.batchSize = 100 ;
   % the default learning rate schedule
-  defaults.learningRate = [0.002, 0.01, 0.02, 0.04 * ones(1,80), 0.004 * ones(1,10), 0.0004 * ones(1,10)] ;
-  defaults.numEpochs = numel(defaults.learningRate) ;
+  if ~opts.batchNorm
+    defaults.learningRate = [0.002, 0.01, 0.02, 0.04 * ones(1,80), 0.004 * ones(1,10), 0.0004 * ones(1,10)] ;
+    defaults.numEpochs = numel(defaults.learningRate) ;
+  else
+    defaults.learningRate = 0.01 ;
+    defaults.numEpochs = 40 ;
+  end
   output.meta = defaults ;
   
 end
 
 function block = ninBlock(in, inChannels, outChannels, ...
-  ker, pad, poolKer, poolMethod, convBlockArgs, final)
+  ker, pad, poolKer, poolMethod, convBlockArgs, bnorm, final)
 
   % get conv block generator with the given options.
   % default activation is ReLU, with post-activation batch normalization.
-  conv = models.ConvBlock('batchNorm', true, convBlockArgs{:}) ;
+  conv = models.ConvBlock('batchNorm', bnorm, convBlockArgs{:}) ;
   
-  % 2 conv blocks
+  % if it's the final layer (prediction), the last conv block has no
+  % activation or batch-norm
+  finalArgs = {} ;
+  if final
+    finalArgs = {'activation', 'none', 'batchNorm', false} ;
+  end
+  
+  % 3 conv blocks
   c1 = conv(in, 'size', [ker(1:2), inChannels, outChannels(1)], 'pad', pad) ;
   c2 = conv(c1, 'size', [1, 1, outChannels(1), outChannels(2)]) ;
-  
+  c3 = conv(c2, 'size', [1, 1, outChannels(2), outChannels(3)], finalArgs{:}) ;
+
+  % pooling
+  p1 = vl_nnpool(c3, poolKer, 'method', poolMethod, 'stride', 2) ;
+
+  % dropout, skipped if it's the final layer (prediction)
   if ~final
-    % third conv block
-    c3 = conv(c2, 'size', [1, 1, outChannels(2), outChannels(3)]) ;
-    
-    % pooling and dropout
-    p1 = vl_nnpool(c3, poolKer, 'method', poolMethod, 'stride', 2) ;
     block = vl_nndropout(p1, 'rate', 0.5) ;
   else
-    % it's the final layer (prediction), no batch-norm/activation/pool
-    block = conv(c2, 'size', [1, 1, outChannels(2), outChannels(3)], ...
-      'batchNorm', false, 'activation', 'none') ;
+    block = p1 ;
   end
 end
 
